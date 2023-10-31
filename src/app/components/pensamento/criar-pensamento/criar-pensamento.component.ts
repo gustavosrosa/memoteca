210c7956be2ddd1b9f81ca653c963eedfc7f55a4
Gustavo Rosa <68282028+gustavodsrosa@users.noreply.github.com>
2023-10-30 23:14:56 -0300

    feature: Criacao bindings

--- src/app/components/pensamento/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamento/criar-pensamento/criar-pensamento.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pensamento } from 'src/app/models/pensamento.model';
 
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
   styleUrls: ['./criar-pensamento.component.css']
 })
-export class CriarPensamentoComponent {
+export class CriarPensamentoComponent implements OnInit {
+
+  pensamento: Pensamento = new Pensamento();
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.initializeComponents();
+  }
+
+  initializeComponents() {
+    this.pensamento.conteudo = "Aqui vai o conteúdo"
+    this.pensamento.autoria = "Aqui vai a autoria"
+  }
 
 }
--- src/app/models/pensamento.model.ts
+++ src/app/models/pensamento.model.ts
@@ -1,6 +1,6 @@
-export interface Pensamento {
-  id: string;
-  conteudo: string;
-  autoria: string;
-  modelo: string;
+export class Pensamento {
+  'id': string;
+  'conteudo': string;
+  'autoria': string;
+  'modelo': string;
 }
